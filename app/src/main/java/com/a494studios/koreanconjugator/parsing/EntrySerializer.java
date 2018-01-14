edb185ad66763e3fa594070f9ebe37424cc36403
Akash Eldo <akasheldo@hotmail.com>
2018-01-13 19:34:15 -0500

    Added Crashlytics logs to Favorites handling and removed old constants Added log messages and keys in EntrySerializer. Now data will be logged whenever getFavorites is called. Removed old constants from Utils as they were no longer being used.

--- app/src/main/java/com/a494studios/koreanconjugator/parsing/EntrySerializer.java
+++ app/src/main/java/com/a494studios/koreanconjugator/parsing/EntrySerializer.java
@@ -1,5 +1,6 @@
 package com.a494studios.koreanconjugator.parsing;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
@@ -19,8 +20,11 @@ public class EntrySerializer implements JsonDeserializer<Map.Entry<String,Catego
     @Override
     public Map.Entry<String, Category[]> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
+        Crashlytics.log("Parsing Json Object: "+jsonObject.toString());
         String key = jsonObject.get("key").getAsString();
+        Crashlytics.setString("Key",key);
         JsonArray value = jsonObject.get("value").getAsJsonArray();
+        Crashlytics.setString("value",value.toString());
         Category[] categories = new Category[3];
         for(int i =0;i<value.size();i++){
             String catString = value.get(i).getAsString();
