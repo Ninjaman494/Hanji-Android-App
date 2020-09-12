d8547e7f1778710de01bd16640229b83dbe37a04
Akash Eldo <akasheldo@hotmail.com>
2020-09-12 12:55:44 -0400

    Scaffolded basic layout

--- app/src/main/java/com/a494studios/koreanconjugator/conjugator/ConjugatorActivity.java
+++ app/src/main/java/com/a494studios/koreanconjugator/conjugator/ConjugatorActivity.java
@@ -1,5 +1,6 @@
 package com.a494studios.koreanconjugator.conjugator;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,11 @@ public class ConjugatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conjugator);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setTitle("Conjugator");
+            actionBar.setElevation(0);
+        }
     }
 }
