433e514226af2651829542135b8425901fc71798
Akash Eldo <akasheldo@hotmail.com>
2020-09-12 18:35:25 -0400

    Conjugator Activity now properly handles loading state Both the conjugations list and the menu are hidden while stems are being fetched. From then on only the conjugations list is hidden when it's being refreshed.

--- app/src/main/java/com/a494studios/koreanconjugator/conjugator/ConjugatorActivity.java
+++ app/src/main/java/com/a494studios/koreanconjugator/conjugator/ConjugatorActivity.java
@@ -2,32 +2,48 @@ package com.a494studios.koreanconjugator.conjugator;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 
+import com.a494studios.koreanconjugator.ConjugationQuery;
 import com.a494studios.koreanconjugator.R;
 import com.a494studios.koreanconjugator.StemQuery;
+import com.a494studios.koreanconjugator.conjugations.ConjugationCardsAdapter;
 import com.a494studios.koreanconjugator.parsing.Server;
 import com.a494studios.koreanconjugator.utils.Utils;
 import com.apollographql.apollo.api.Response;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
 
 import io.reactivex.observers.DisposableObserver;
 
-public class ConjugatorActivity extends AppCompatActivity {
+public class ConjugatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     public static final String EXTRA_TERM = "term";
     private static final int SPINNER_ITEM = R.layout.item_spinner;
 
+    private RecyclerView recyclerView;
     private LinearLayout linearLayout;
     private ProgressBar loadingBar;
+    private Spinner stemSpinner;
+    private Spinner posSpinner;
+    private Spinner regSpinner;
+
+    // Used to prevent multiple refreshes for the same data
+    private boolean isRefreshing = false;
+    // Used to hide fields when first fetching possible stems
+    private boolean fetchingStems = false;
 
     @SuppressLint("CheckResult")
     @Override
@@ -43,11 +59,16 @@ public class ConjugatorActivity extends AppCompatActivity {
             actionBar.setElevation(0);
         }
 
+        recyclerView = findViewById(R.id.conjugator_list);
         linearLayout = findViewById(R.id.conjugator_linearLayout);
         loadingBar = findViewById(R.id.conjugator_progress);
-        Spinner stemSpinner = findViewById(R.id.conjugator_stemSpinner);
-        Spinner posSpinner = findViewById(R.id.conjugator_posSpinner);
-        Spinner regSpinner = findViewById(R.id.conjugator_regSpinner);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        stemSpinner = findViewById(R.id.conjugator_stemSpinner);
+        posSpinner = findViewById(R.id.conjugator_posSpinner);
+        regSpinner = findViewById(R.id.conjugator_regSpinner);
 
         ArrayAdapter posAdapter = ArrayAdapter.createFromResource(this,
                 R.array.pos, SPINNER_ITEM);
@@ -57,6 +78,11 @@ public class ConjugatorActivity extends AppCompatActivity {
         posSpinner.setAdapter(posAdapter);
         regSpinner.setAdapter(regAdapter);
 
+        stemSpinner.setOnItemSelectedListener(this);
+        posSpinner.setOnItemSelectedListener(this);
+        regSpinner.setOnItemSelectedListener(this);
+
+        fetchingStems = true;
         setLoading(true);
 
         Server.doStemQuery(term)
@@ -74,7 +100,7 @@ public class ConjugatorActivity extends AppCompatActivity {
                             stemSpinner.setAdapter(adapter);
                             stemSpinner.setEnabled(true);
 
-                            setLoading(false);
+                            fetchingStems = false;
                         });
                     }
 
@@ -94,12 +120,61 @@ public class ConjugatorActivity extends AppCompatActivity {
 
     }
 
+    @SuppressLint("CheckResult")
+    private void getConjugations(String stem, boolean honorific, boolean isAdj, Boolean regular) {
+        Server.doConjugationQuery(stem, honorific, isAdj, regular)
+                .subscribeWith(new DisposableObserver<Response<ConjugationQuery.Data>>() {
+                    @Override
+                    public void onNext(Response<ConjugationQuery.Data> response) {
+                        if(response.data() == null) {
+                            return;
+                        }
+
+                        List<ConjugationQuery.Conjugation> conjugations = response.data().conjugations();
+                        final TreeMap<String,List<ConjugationQuery.Conjugation>> conjMap = new TreeMap<>();
+                        for(ConjugationQuery.Conjugation conjugation : conjugations){
+                            String type = conjugation.type();
+                            if(conjMap.containsKey(type)){
+                                conjMap.get(type).add(conjugation);
+                            }else{
+                                List<ConjugationQuery.Conjugation> value = new ArrayList<>();
+                                value.add(conjugation);
+                                conjMap.put(type,value);
+                            }
+                        }
+
+                        List<List<ConjugationQuery.Conjugation>> conjugations1 = new ArrayList<>(conjMap.values());
+                        recyclerView.setAdapter(new ConjugationCardsAdapter(conjugations1, stem, isAdj ? "Adjective" : "Verb"));
+                        setLoading(false);
+                        isRefreshing = false;
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                        Utils.handleError(e, ConjugatorActivity.this,4,
+                                (dialogInterface, i) -> ConjugatorActivity.this.onBackPressed());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
     private void setLoading(boolean loading){
         if(loading) {
             loadingBar.setVisibility(View.VISIBLE);
-            linearLayout.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.GONE);
+
+            if(fetchingStems) {
+                linearLayout.setVisibility(View.INVISIBLE);
+            }
+
         } else {
             loadingBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
             linearLayout.setVisibility(View.VISIBLE);
 /*
             if(dataLoaded) {
@@ -108,5 +183,31 @@ public class ConjugatorActivity extends AppCompatActivity {
                 animationHandler.animateListView();
             }*/
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Object stemItem = stemSpinner.getSelectedItem();
+        Object posItem = posSpinner.getSelectedItem();
+        Object regItem = regSpinner.getSelectedItem();
+
+        // Already refreshing conjugations, wait for that to finish first, or not
+        // all spinners have finished being set up
+        if (isRefreshing || stemItem == null || posItem == null || regItem == null) {
+            return;
+        }
+
+        isRefreshing = true;
+        setLoading(true);
+
+        String stem = stemItem.toString();
+        boolean isAdj = posItem.toString().equals("Adjective");
+        boolean regular = regItem.toString().equals("Regular verb/adjective");
+        getConjugations(stem, false, isAdj, regular);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
