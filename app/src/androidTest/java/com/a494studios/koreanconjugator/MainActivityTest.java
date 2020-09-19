1a904bffab66d53c41ccd3167c9b934f2be0a867
Akash Eldo <akasheldo@hotmail.com>
2020-09-19 10:20:10 -0400

    Created an instrumented test for Main Activity

---
+++ app/src/androidTest/java/com/a494studios/koreanconjugator/MainActivityTest.java
@@ -0,0 +1,63 @@
+package com.a494studios.koreanconjugator;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.a494studios.koreanconjugator.display.DisplayActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.a494studios.koreanconjugator.Utils.withHint;
+import static com.google.common.base.CharMatcher.any;
+import static org.hamcrest.Matchers.allOf;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Test
+    public void test_view() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+
+        onView(withId(R.id.searchCard_search))
+                .check(matches(isDisplayed()))
+                .check(matches(withHint("Search in Korean or English…")));
+
+        onView(withId(R.id.main_wodCard))
+                .check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.displayCard_heading), isDescendantOfA(withId(R.id.main_wodCard))))
+                .check(matches(withText("Word of the Day")));
+    }
+
+    @Test
+    public void test_wodCard_redirects_to_display() {
+        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+
+        onView(withId(R.id.main_wodCard))
+                .check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.displayCard_button), isDescendantOfA(withId(R.id.main_wodCard))))
+                .perform(click());
+
+
+
+        intended(hasExtra(DisplayActivity.EXTRA_ID, any()));
+        intended(hasComponent(DisplayActivity.class.getName()));
+    }
+}
--- app/src/androidTest/java/com/a494studios/koreanconjugator/Utils.java
+++ app/src/androidTest/java/com/a494studios/koreanconjugator/Utils.java
@@ -3,11 +3,14 @@ package com.a494studios.koreanconjugator;
 import android.view.View;
 import android.widget.Checkable;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 import static org.hamcrest.Matchers.isA;
 
@@ -42,6 +45,26 @@ class Utils {
             public void perform(UiController uiController, View view) {
                 Checkable checkableView = (Checkable) view;
                 checkableView.setChecked(checked);
+            }
+        };
+    }
+
+    public static Matcher<View> withHint(final String expectedHint) {
+        return new TypeSafeMatcher<View>() {
+
+            @Override
+            public boolean matchesSafely(View view) {
+                if (!(view instanceof SearchView)) {
+                    return false;
+                }
+
+                String hint = ((SearchView) view).getQueryHint().toString();
+
+                return expectedHint.equals(hint);
+            }
+
+            @Override
+            public void describeTo(Description description) {
             }
         };
     }
