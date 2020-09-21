18d76770f8440beb9009087557e3292bb01257d1
Akash Eldo <akasheldo@hotmail.com>
2020-09-20 20:12:05 -0400

    Finished tests for Main Activity

--- app/src/androidTest/java/com/a494studios/koreanconjugator/MainActivityTest.java
+++ app/src/androidTest/java/com/a494studios/koreanconjugator/MainActivityTest.java
@@ -16,13 +16,12 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.a494studios.koreanconjugator.Utils.withHint;
-import static com.google.common.base.CharMatcher.any;
 import static org.hamcrest.Matchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
@@ -34,7 +33,7 @@ public class MainActivityTest {
 
     @Test
     public void test_view() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        ActivityScenario.launch(MainActivity.class);
 
         onView(withId(R.id.searchCard_search))
                 .check(matches(isDisplayed()))
@@ -48,16 +47,14 @@ public class MainActivityTest {
 
     @Test
     public void test_wodCard_redirects_to_display() {
-        ActivityScenario<MainActivity> scenario = ActivityScenario.launch(MainActivity.class);
+        ActivityScenario.launch(MainActivity.class);
 
         onView(withId(R.id.main_wodCard))
                 .check(matches(isDisplayed()));
         onView(allOf(withId(R.id.displayCard_button), isDescendantOfA(withId(R.id.main_wodCard))))
                 .perform(click());
 
-
-
-        intended(hasExtra(DisplayActivity.EXTRA_ID, any()));
-        intended(hasComponent(DisplayActivity.class.getName()));
+        intended(allOf(hasExtraWithKey(DisplayActivity.EXTRA_ID),
+                hasComponent(DisplayActivity.class.getName())));
     }
 }
