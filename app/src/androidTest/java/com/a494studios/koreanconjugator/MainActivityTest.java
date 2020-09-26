822bd65053905aa0a06b3aebd750c4dff4fcbf65
Akash Eldo <akasheldo@hotmail.com>
2020-09-26 14:50:49 -0400

    Updated other Main Activity tests and Simplified them

--- app/src/androidTest/java/com/a494studios/koreanconjugator/MainActivityTest.java
+++ app/src/androidTest/java/com/a494studios/koreanconjugator/MainActivityTest.java
@@ -4,16 +4,11 @@ import android.app.Activity;
 import android.app.Instrumentation;
 import android.app.SearchManager;
 import android.content.Intent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import androidx.test.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.IdlingRegistry;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.idling.CountingIdlingResource;
 import androidx.test.espresso.intent.Intents;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -22,10 +17,6 @@ import com.a494studios.koreanconjugator.display.DisplayActivity;
 import com.a494studios.koreanconjugator.parsing.Server;
 import com.a494studios.koreanconjugator.search.SearchActivity;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -33,8 +24,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.URL;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -49,16 +38,12 @@ import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.a494studios.koreanconjugator.Utils.testActionBar;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 
 @LargeTest
@@ -77,114 +62,67 @@ public class MainActivityTest {
             "}";
 
     @Rule
+    @SuppressWarnings("deprecation")
     public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class, true, false);
+    private MockWebServer server;
 
     @Before
-    public void stubIntents() {
-        // Espresso Idling Resource registration
+    public void stubIntents() throws IOException {
+        // Register idling resource
         IdlingRegistry.getInstance().register(idler);
 
+        // Stub intents
         Intents.init();
-
         Intent resultData = new Intent();
         Instrumentation.ActivityResult result = new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
-        intending(allOf(isInternal(), not(hasComponent(MainActivity.class.getName())))).respondWith(result);
-    }
+        intending(not(hasComponent(MainActivity.class.getName()))).respondWith(result);
 
-    @After
-    public void releaseIntents() {
-        Intents.release();
-        IdlingRegistry.getInstance().unregister(idler);
-    }
+        // Setup mock server
+        server = new MockWebServer();
+        server.start();
+        MockApplication testApp = ApplicationProvider.getApplicationContext();
+        testApp.setServerUrl(server.url("/").toString());
 
-    @Test
-    public void overflowOptions() {
-        testActionBar(false);
-    }
-
-    @Test
-    public void wodCard_checkTitle() {
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.displayCard_heading), withText("Word of the Day"),
-                        childAtPosition(
-                                allOf(withId(R.id.displayCard_relativeLayout),
-                                        childAtPosition(
-                                                IsInstanceOf.instanceOf(android.widget.FrameLayout.class),
-                                                0)),
-                                0),
-                        isDisplayed()));
-        textView.check(matches(withText("Word of the Day")));
-    }
-
-    @Test
-    public void wodCard_redirectsToDisplay() throws IOException {
-        MockWebServer server = mockWODQuery();
+        // Enqueue a response
+        server.enqueue(new MockResponse().setBody(WOD_RESPONSE));
 
         // Start test
         activityRule.launchActivity(null);
+    }
 
-        ViewInteraction wodButton = onView(withText("See Entry"));
-        wodButton.check(matches(isDisplayed()));
-        wodButton.perform(click());
-
-        intended(allOf(hasComponent(DisplayActivity.class.getName()),
-                hasExtra(DisplayActivity.EXTRA_ID, "가로0")));
-
+    @After
+    public void releaseIntents() throws IOException {
+        Intents.release();
+        IdlingRegistry.getInstance().unregister(idler);
         server.shutdown();
     }
 
     @Test
+    public void overflowOptions() {
+        testActionBar();
+    }
+
+    @Test
+    public void wodCard_redirectsToDisplay() {
+        onView(withText("Word of the Day"))
+                .check(matches(isDisplayed()));
+
+        onView(withText("See Entry"))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        intended(allOf(hasComponent(DisplayActivity.class.getName()),
+                hasExtra(DisplayActivity.EXTRA_ID, "가로0")));
+    }
+
+    @Test
     public void search_redirectsToSearch() {
-        ViewInteraction searchAutoComplete = onView(
-                allOf(withId(R.id.search_src_text),
-                        childAtPosition(
-                                allOf(withId(R.id.search_plate),
-                                        childAtPosition(
-                                                withId(R.id.search_edit_frame),
-                                                1)),
-                                0),
-                        isDisplayed()));
-
-        searchAutoComplete.check(matches(withHint("Search in Korean or English…")));
-
-        searchAutoComplete.perform(replaceText("text"), pressImeActionButton());
+        onView(withId(R.id.search_src_text))
+                .check(matches(withHint("Search in Korean or English…")))
+                .perform(replaceText("text"), pressImeActionButton());
 
         intended(allOf(hasComponent(SearchActivity.class.getName()),
                     hasAction(Intent.ACTION_SEARCH),
-                    hasExtraWithKey(SearchManager.QUERY)));
-    }
-
-    static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
-
-    private MockWebServer mockWODQuery() throws IOException {
-        // Setup mock server
-        MockWebServer server = new MockWebServer();
-        server.start();
-
-        // Set the base url of the test app using the url of the mocked local server
-        MockApplication testApp = (MockApplication) InstrumentationRegistry.getTargetContext().getApplicationContext();
-        testApp.setServerUrl(server.url("/").toString());
-
-        // Enqueue the response
-        server.enqueue(new MockResponse().setBody(WOD_RESPONSE));
-
-        return server;
+                    hasExtra(SearchManager.QUERY, "text")));
     }
 }
--- app/src/androidTest/java/com/a494studios/koreanconjugator/Utils.java
+++ app/src/androidTest/java/com/a494studios/koreanconjugator/Utils.java
@@ -3,7 +3,6 @@ package com.a494studios.koreanconjugator;
 import android.view.View;
 import android.widget.Checkable;
 
-import androidx.appcompat.widget.SearchView;
 import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
@@ -13,20 +12,14 @@ import com.eggheadgames.aboutbox.activity.AboutActivity;
 
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.rm3l.maoni.ui.MaoniActivity;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static com.a494studios.koreanconjugator.MainActivityTest.childAtPosition;
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.isA;
 
 class Utils {
@@ -66,52 +59,21 @@ class Utils {
         };
     }
 
-    public static void testActionBar(boolean hasSearch) {
-        ViewInteraction overflowMenuButton = onView(
-                allOf(withContentDescription("More options"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.action_bar),
-                                        0),
-                                hasSearch ? 1 : 0),
-                        isDisplayed()));
+    public static void testActionBar() {
+        ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
 
         overflowMenuButton.perform(click());
-        ViewInteraction settings = onView(
-                allOf(withId(R.id.title), withText("Settings"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        settings.perform(click());
+        onView(withText("Settings")).perform(click());
 
         intended(hasComponent(SettingsActivity.class.getName()));
 
         overflowMenuButton.perform(click());
-        ViewInteraction about = onView(
-                allOf(withId(R.id.title), withText("About"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        about.perform(click());
+        onView(withText("About")).perform(click());
 
         intended(hasComponent(AboutActivity.class.getName()));
 
         overflowMenuButton.perform(click());
-        ViewInteraction reportABug = onView(
-                allOf(withId(R.id.title), withText("Report a Bug"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.content),
-                                        0),
-                                0),
-                        isDisplayed()));
-        reportABug.perform(click());
+        onView(withText("Report a Bug")).perform(click());
 
         intended(hasComponent(MaoniActivity.class.getName()));
     }
