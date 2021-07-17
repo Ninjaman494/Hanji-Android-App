ab19864a15e2aa47d87d0d9530810f4cda8ffdef
Akash Eldo <akasheldo@hotmail.com>
2021-07-16 21:31:23 -0400

    Fixed AdCard test

--- app/src/test/java/com/a494studios/koreanconjugator/display/cards/AdCardUnitTest.java
+++ app/src/test/java/com/a494studios/koreanconjugator/display/cards/AdCardUnitTest.java
@@ -1,8 +1,10 @@
 package com.a494studios.koreanconjugator.display.cards;
 
-import android.app.Activity;
+import android.content.Context;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+
+import androidx.test.core.app.ApplicationProvider;
 
 import com.a494studios.koreanconjugator.R;
 import com.a494studios.koreanconjugator.display.DisplayCardView;
@@ -10,36 +12,35 @@ import com.a494studios.koreanconjugator.display.DisplayCardView;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.LooperMode;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(RobolectricTestRunner.class)
+@LooperMode(LooperMode.Mode.PAUSED)
 public class AdCardUnitTest {
     private AdCard card;
-    private Activity activity;
     private DisplayCardView cardView;
     private LinearLayout viewGroup;
 
     @Before
     public void init() {
+        Context context = ApplicationProvider.getApplicationContext();
         card = new AdCard("Ad unit id");
-        activity = Robolectric.buildActivity(Activity.class).create().start().visible().get();
-        cardView = new DisplayCardView(activity);
-        viewGroup = new LinearLayout(activity);
+        cardView = new DisplayCardView(context);
+        viewGroup = new LinearLayout(context);
     }
 
     @Test
     public void test_shouldHideButton() {
-        card.addBodyView(activity, viewGroup, cardView);
-
+        card.addBodyView(ApplicationProvider.getApplicationContext(), viewGroup, cardView);
         assertEquals(cardView.findViewById(R.id.displayCard_button).getVisibility(), ViewGroup.GONE);
     }
 
     @Test
     public void test_getCount() {
-        assertEquals(1,card.getCount());
+        assertEquals(1, card.getCount());
     }
 
     @Test
